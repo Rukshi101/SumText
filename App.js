5873f099e4b452b48aeb6bc859277fbd43ecb819
Rukshi101 <rukshithiyagayogan@gmail.com>
2020-08-17 20:47:51 -0400

    commit

--- App.js
+++ App.js
@@ -25,12 +25,38 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Camera, {Constants} from './src/components/camera';
+import RNFS from 'react-native-fs';
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCamera:false,
+
+    }
+  }
   render(){
     return(
   
   <View>
     <Text>OKAY</Text>
+    <Button onClick = {() =>  this.setstate({showCamera :true})} > BUTTON</Button>
+    {
+      this.state.showCamera && 
+      <Camera 
+      cameraType = {Constants.Type.front}
+      flashMode = {Constants.FlashMode.off}
+      autoFocus ={Constants.AutoFocus.on}
+      whiteBalance = {Constants.WhiteBalance.auto}
+      ratio = {'1:1'}
+      quality = {0.5}
+      imageWidth = {800}
+      onCapture = {data => this.savePhoto(data)}
+      onClose = {_ =>{
+        this.setState({showCamera:false})
+      }}
+      />
+    }
   </View>
     )
   }
