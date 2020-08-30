5b4884e242891f640b61e622aa5477d0d0a8e86c
Rohan Peri <perirohan@gmail.com>
2020-08-30 16:01:02 -0700

    Adding saveScreen

---
+++ src/components/saveScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight,
+  SectionList,
+} from 'react-native';
+
+import PropTypes from 'prop-types';
+import LocalDB from '../lib/localDB';
+
+class saveScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+export default saveScreen;
