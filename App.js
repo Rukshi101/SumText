b09f8163bf22915c13ffbd2804f9e8182985ce75
Rohan Peri <perirohan@gmail.com>
2020-08-31 09:38:32 -0700

    Code formatting and cleanup

--- App.js
+++ App.js
@@ -33,46 +33,44 @@ import RNFS from 'react-native-fs';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from "@react-navigation/stack"
+import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './src/components/HomeScreen';
 import CameraScreen from './src/components/CameraScreen';
 import BookmarkScreen from './src/components/BookMarkScreen';
-import UploadScreen from './src/components/UploadScreen'
+import UploadScreen from './src/components/UploadScreen';
 import PreviewScreen from './src/components/PreviewScreen';
 
 const Tab = createBottomTabNavigator();
 
-const HomeStack =createStackNavigator();
-function HomeStackScreen(){
-  return(
+const HomeStack = createStackNavigator();
+function HomeStackScreen() {
+  return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name = "Home" component = {HomeScreen}/>
-      <HomeStack.Screen name = "Bookmarks" component = {BookmarkScreen}/>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Bookmarks" component={BookmarkScreen} />
     </HomeStack.Navigator>
-  )
+  );
 }
 
-
-const BookMarkStack =createStackNavigator();
-function BookMarkStackScreen(){
-  return(
+const BookMarkStack = createStackNavigator();
+function BookMarkStackScreen() {
+  return (
     <BookMarkStack.Navigator>
-      <BookMarkStack.Screen name = "Bookmarkss" component = {BookmarkScreen}/>
+      <BookMarkStack.Screen name="Bookmarkss" component={BookmarkScreen} />
     </BookMarkStack.Navigator>
-  )
+  );
 }
 
 class App extends React.Component {
   render() {
     return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name = "Home" component = {HomeStackScreen}/>
-        <Tab.Screen name = "Camera" component = {CameraScreen}/>
-        <Tab.Screen name = "Upload" component = {UploadScreen}/>
-        <Tab.Screen name = "Bookmarked" component = {BookMarkStackScreen}/>
-        
-      </Tab.Navigator>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeStackScreen} />
+          <Tab.Screen name="Camera" component={CameraScreen} />
+          <Tab.Screen name="Upload" component={UploadScreen} />
+          <Tab.Screen name="Bookmarked" component={BookMarkStackScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     );
   }
